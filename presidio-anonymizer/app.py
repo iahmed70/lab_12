63f8851dedda9a1594e368ffbcdb57adbd59b2ab
Ishaq Ahmed <iahmed70@users.noreply.github.com>
2025-12-08 23:23:09 +0000

    updates:

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -6,12 +6,11 @@ from logging.config import fileConfig
 from pathlib import Path
 
 from flask import Flask, Response, jsonify, request
-from werkzeug.exceptions import BadRequest, HTTPException
-
 from presidio_anonymizer import AnonymizerEngine, DeanonymizeEngine
 from presidio_anonymizer.entities import InvalidParamError, OperatorConfig
 from presidio_anonymizer.operators.genz import GenZ
 from presidio_anonymizer.services.app_entities_convertor import AppEntitiesConvertor
+from werkzeug.exceptions import BadRequest, HTTPException
 
 DEFAULT_PORT = "3000"
 
